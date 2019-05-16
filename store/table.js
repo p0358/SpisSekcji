5ad0e2eca68eb013b825506ef9748ff4a4303dfb
p0358 <p0358@users.noreply.github.com>
2019-05-16 21:32:14 +0200

    Add smart sorting

--- store/table.js
+++ store/table.js
@@ -6,7 +6,92 @@ export const state = () => ({
       label: "Nazwa",
       field: "name",
       align: "left",
-      sortable: true
+      sortable: true,
+      sort: (a, b, rowA, rowB) => {
+        const lettermap = {
+          Ё: "YO",
+          Й: "I",
+          Ц: "TS",
+          У: "U",
+          К: "K",
+          Е: "E",
+          Н: "N",
+          Г: "G",
+          Ш: "SH",
+          Щ: "SCH",
+          З: "Z",
+          Х: "H",
+          Ъ: "'",
+          ё: "yo",
+          й: "i",
+          ц: "ts",
+          у: "u",
+          к: "k",
+          е: "e",
+          н: "n",
+          г: "g",
+          ш: "sh",
+          щ: "sch",
+          з: "z",
+          х: "h",
+          ъ: "'",
+          Ф: "F",
+          Ы: "I",
+          В: "V",
+          А: "a",
+          П: "P",
+          Р: "R",
+          О: "O",
+          Л: "L",
+          Д: "D",
+          Ж: "ZH",
+          Э: "E",
+          ф: "f",
+          ы: "i",
+          в: "v",
+          а: "a",
+          п: "p",
+          р: "r",
+          о: "o",
+          л: "l",
+          д: "d",
+          ж: "zh",
+          э: "e",
+          Я: "Ya",
+          Ч: "CH",
+          С: "S",
+          М: "M",
+          И: "I",
+          Т: "T",
+          Ь: "'",
+          Б: "B",
+          Ю: "YU",
+          я: "ya",
+          ч: "ch",
+          с: "s",
+          м: "m",
+          и: "i",
+          т: "t",
+          ь: "'",
+          б: "b",
+          ю: "yu"
+        };
+        const transliterate = function(word) {
+          return word
+            .split("")
+            .map(function(char) {
+              return lettermap[char] || char;
+            })
+            .join("");
+        };
+        [a, b] = [a, b].map(s =>
+          transliterate(s + "")
+            .toLowerCase()
+            .replace(/[^a-z0-9 ]/gi, "")
+            .trim()
+        );
+        return a > b ? 1 : a === b ? 0 : -1;
+      }
     },
     {
       name: "Members",
